63407a191d308ba809422522731949e0d3250bfc
GuiMayor <villawolflobo@gmail.com>
2025-07-22 18:41:04 +0200

    post crear libro terminado

--- controllers/book_controllers.py
+++ controllers/book_controllers.py
@@ -9,14 +9,22 @@ from models.book_models import Book, BookCreate
 def book_helper(book:dict) ->Book:
     return Book(
         id=str(book["_id"]),
-        title=book("title"),
-        author=book('author'),
-        year=book('year'),
+        title=book["title"],
+        author=book['author'],
+        year=book['year'],
         pages=book.get('pages'),
         
     )
+
+
 async def create_book(book:BookCreate):
     try:
+
         new_book=book.model_dump()
-        result = await book_collection.insert_one(new_book)
+        result = await  book_collection.insert_one(new_book)
+        book_created = await book_collection.find_one({"_id": result.inserted_id})
+        return book_helper(book_created)
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=f'Error:{str(e)}')
+    
         
