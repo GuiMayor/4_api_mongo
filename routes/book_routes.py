54d6feec8b1ec6efad858207360c771e81e43fb7
GuiMayor <villawolflobo@gmail.com>
2025-07-23 01:32:24 +0200

    ruta put update terminada

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -22,3 +22,6 @@ async def get_book_by_id(book_id:str):
     return await book_controllers.get_book_by_id(book_id)
 
 
+@router.put("/{book_id}",status_code=200)
+async def update_book(book_id:str):
+    return await book_controllers.update_book(book_id)
