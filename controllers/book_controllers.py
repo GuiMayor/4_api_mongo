ad8da523220d9f16035734af2c78ff91b300c2c3
GuiMayor <villawolflobo@gmail.com>
2025-07-23 02:09:12 +0200

    update finish

--- controllers/book_controllers.py
+++ controllers/book_controllers.py
@@ -53,3 +53,17 @@ async def get_book_by_id(book_id:str):
     except Exception as e:
         raise HTTPException(status_code=500,detail=f'Error:{str(e)}')
         
+async def update_book(book_id:str, book_data:Book):
+    try:
+        if not ObjectId.is_valid(book_id):
+            raise HTTPException(status_code=400, detail="id del libro no es valido")
+        result = await book_collection.update_one(
+            {"_id":ObjectId(book_id)},
+            {"$set": book_data.model_dump()}
+            )
+        if result.modified_count==0:
+            return None
+        update= await book_collection.find_one({"_id":ObjectId(book_id)})
+        return book_helper(update)
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=f'Error:{str(e)}')
